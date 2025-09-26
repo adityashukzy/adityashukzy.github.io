c6afa17e6c2ae33b7fbd6d54f8363ec833b748c7
Aditya Shukla <adityashukzy@gmail.com>
2025-09-26 12:32:38 -0400

    Fix bolding

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -11,13 +11,13 @@ export default function PublicationsPage() {
       <div className="space-y-6">
         <div>
           <h2 className="font-medium">An Integrated Document Manipulation Tool Leveraging LLMs, Transformers, and Attention Mechanism</h2>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400"><bold>Aditya Shukla</bold>, Pooja Ravi, B. Arthi</p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400"><strong>Aditya Shukla</strong>, Pooja Ravi, B. Arthi</p>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">ICIOT 2023 | AIP Conference Proceedings 2024</p>
           <a href="https://doi.org/10.1063/5.0217040" className="text-sm">PDF</a>
         </div>
         <div>
           <h2 className="font-medium">Real-Time GPU-Accelerated Driver Assistance System</h2>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">Pooja Ravi, <bold>Aditya Shukla</bold>, and B. Muruganantham</p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">Pooja Ravi, <strong>Aditya Shukla</strong>, and B. Muruganantham</p>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">RCAAI 2022, Springer - Intelligent Control, Robotics, and Industrial Automation 2023</p>
           <a href="#https://link.springer.com/chapter/10.1007/978-981-99-4634-1_64" className="text-sm">PDF</a>
         </div>
